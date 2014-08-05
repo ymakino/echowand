9cff582031c3cd57b62cf2cb11ad8429dc5ab298
Yoshiki Makino <ymakino_@hotmail.com>
2014-08-05 20:14:05 +0900

    Updated some comments.

--- src/echowand/net/NetworkException.java
+++ src/echowand/net/NetworkException.java
@@ -1,13 +1,13 @@
 package echowand.net;
 
 /**
- * コネクション関係の例外
+ * フレームの送受信関係の例外
  * @author ymakino
  */
 public class NetworkException extends Exception {
     
     /**
-     * Connectionに関する例外を表すオブジェクトを生成する。
+     * フレームの送受信に関する例外を表すオブジェクトを生成する。
      * @param message 例外に関する情報
      */
     public NetworkException(String message) {
@@ -15,7 +15,7 @@ public class NetworkException extends Exception {
     }
     
     /**
-     * Connectionに関する例外を表すオブジェクトを生成する。
+     * フレームの送受信に関する例外を表すオブジェクトを生成する。
      * @param message 例外に関する情報
      * @param cause この例外の発生原因となった例外
      */
