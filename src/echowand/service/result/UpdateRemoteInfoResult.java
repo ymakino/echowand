7bdab75f4b8e6a7d5e5f2fce4c9f99f64b6ebe70
Yoshiki Makino <ymakino_@hotmail.com>
2015-03-29 14:21:50 +0900

    Added some methods and updated getFrame method.

--- src/echowand/service/result/UpdateRemoteInfoResult.java
+++ src/echowand/service/result/UpdateRemoteInfoResult.java
@@ -10,6 +10,7 @@ import echowand.net.StandardPayload;
 import echowand.object.InstanceListRequestExecutor;
 import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.logging.Logger;
 
 /**
@@ -19,177 +20,286 @@ import java.util.logging.Logger;
 public class UpdateRemoteInfoResult {
     private static final Logger LOGGER = Logger.getLogger(UpdateRemoteInfoResult.class.getName());
     private static final String CLASS_NAME = UpdateRemoteInfoResult.class.getName();
-    
+
     private InstanceListRequestExecutor executor;
-    private LinkedList<Frame> frames;
-    private LinkedList<Frame> errorFrames;
-    private LinkedList<Frame> invalidFrames;
-    private LinkedList<Node> nodes;
+    private LinkedList<ResultFrame> frameList;
+    private LinkedList<ResultFrame> errorFrameList;
+    private LinkedList<ResultFrame> invalidFrameList;
+    private LinkedList<Node> nodeList;
     private HashMap<Node, LinkedList<EOJ>> nodeEOJMap;
-    
+
     public UpdateRemoteInfoResult(InstanceListRequestExecutor executor) {
         LOGGER.entering(CLASS_NAME, "UpdateRemoteInfoResult", executor);
-        
+
         this.executor = executor;
-        frames = new LinkedList<Frame>();
-        errorFrames = new LinkedList<Frame>();
-        invalidFrames = new LinkedList<Frame>();
-        nodes = new LinkedList<Node>();
+        frameList = new LinkedList<ResultFrame>();
+        errorFrameList = new LinkedList<ResultFrame>();
+        invalidFrameList = new LinkedList<ResultFrame>();
+        nodeList = new LinkedList<Node>();
         nodeEOJMap = new HashMap<Node, LinkedList<EOJ>>();
-        
+
         LOGGER.exiting(CLASS_NAME, "UpdateRemoteInfoResult");
     }
-    
+
     public boolean isDone() {
         LOGGER.entering(CLASS_NAME, "isDone");
-        
+
         boolean result = executor.isDone();
-        
+
         LOGGER.exiting(CLASS_NAME, "isDone", result);
         return result;
     }
-    
+
     public void join() throws InterruptedException {
         LOGGER.entering(CLASS_NAME, "join");
-        
+
         executor.join();
-        
+
         LOGGER.exiting(CLASS_NAME, "join");
     }
-    
+
     private int addEOJs(Node node, StandardPayload payload) {
-        int count = 0;
+        LOGGER.entering(CLASS_NAME, "addEOJs", new Object[]{node, payload});
         
+        int count = 0;
+
         if (payload.getESV() != ESV.Get_Res || !payload.getSEOJ().isNodeProfileObject()) {
+            LOGGER.exiting(CLASS_NAME, "addEOJs", count);
             return count;
         }
-        
+
         addEOJ(node, payload.getSEOJ());
-        
+
         for (int i = 0; i < payload.getFirstOPC(); i++) {
             if (payload.getFirstPropertyAt(i).getEPC() == EPC.xD6) {
                 Data data = payload.getFirstPropertyAt(i).getEDT();
-                
+
                 if (data.isEmpty()) {
                     continue;
                 }
-                
+
                 int size = 0xff & data.get(0);
-                
+
                 if (data.size() - 1 != size * 3) {
                     continue;
                 }
-                
-                for (int j=0; j<size; j++) {
+
+                for (int j = 0; j < size; j++) {
                     int offset = 1 + j * 3;
-                    
+
                     byte classGroupCode = data.get(offset);
-                    byte classCode = data.get(offset+1);
-                    byte instanceCode = data.get(offset+2);
-                    
+                    byte classCode = data.get(offset + 1);
+                    byte instanceCode = data.get(offset + 2);
+
                     addEOJ(node, new EOJ(classGroupCode, classCode, instanceCode));
                     count++;
                 }
             }
         }
-        
+
+        LOGGER.exiting(CLASS_NAME, "addEOJs", count);
         return count;
     }
-    
+
     private void addEOJ(Node node, EOJ eoj) {
+        LOGGER.exiting(CLASS_NAME, "addEOJ", new Object[]{node, eoj});
+            
         if (!nodeEOJMap.containsKey(node)) {
-            nodes.add(node);
+            nodeList.add(node);
             nodeEOJMap.put(node, new LinkedList<EOJ>());
         }
+
+        LinkedList<EOJ> eojs = nodeEOJMap.get(node);
+
+        if (eojs == null) {
+            eojs = new LinkedList<EOJ>();
+        }
+
+        if (!eojs.contains(eoj)) {
+            eojs.add(eoj);
+        }
+
+        nodeEOJMap.put(node, eojs);
         
+        LOGGER.exiting(CLASS_NAME, "addEOJ");
+    }
+
+    public synchronized boolean addFrame(Frame frame) {
+        LOGGER.entering(CLASS_NAME, "addFrame", frame);
+
+        boolean result = addFrame(createResultFrame(frame));
+
+        LOGGER.exiting(CLASS_NAME, "addFrame", result);
+        return result;
+    }
+
+    public synchronized boolean addFrame(ResultFrame resultFrame) {
+        LOGGER.entering(CLASS_NAME, "addFrame", resultFrame);
+
+        Frame frame = resultFrame.frame;
+
+        if (!frame.getCommonFrame().isStandardPayload()) {
+            invalidFrameList.add(resultFrame);
+            LOGGER.exiting(CLASS_NAME, "addFrame", false);
+            return false;
+        }
+
+        if (!(frame.getCommonFrame().getEDATA() instanceof StandardPayload)) {
+            invalidFrameList.add(resultFrame);
+            LOGGER.exiting(CLASS_NAME, "addFrame", false);
+            return false;
+        }
+
+        Node node = frame.getSender();
+        StandardPayload payload = (StandardPayload) frame.getCommonFrame().getEDATA();
+
+        if (payload.getESV() != ESV.Get_Res && payload.getESV() != ESV.Get_SNA) {
+            invalidFrameList.add(resultFrame);
+            LOGGER.exiting(CLASS_NAME, "addFrame", false);
+            return false;
+        }
+
+        addEOJs(node, payload);
+
+        if (payload.getESV() == ESV.Get_SNA) {
+            errorFrameList.add(resultFrame);
+        }
+
+        boolean result = frameList.add(resultFrame);
+        LOGGER.exiting(CLASS_NAME, "addFrame", result);
+        return result;
+    }
+
+    private synchronized ResultFrame createResultFrame(Frame frame) {
+        LOGGER.entering(CLASS_NAME, "createResultFrame", frame);
+
+        long time = System.currentTimeMillis();
+        ResultFrame resultFrame = new ResultFrame(frame, time);
+
+        LOGGER.exiting(CLASS_NAME, "createResultFrame", resultFrame);
+        return resultFrame;
+    }
+
+    public synchronized int countFrames() {
+        LOGGER.entering(CLASS_NAME, "countFrames");
+
+        int count = frameList.size();
+        LOGGER.exiting(CLASS_NAME, "countFrames", count);
+        return count;
+    }
+
+    public synchronized ResultFrame getFrame(int index) {
+        LOGGER.entering(CLASS_NAME, "getFrame", index);
+
+        ResultFrame resultFrame = frameList.get(index);
+        LOGGER.exiting(CLASS_NAME, "getFrame", resultFrame);
+        return resultFrame;
+    }
+    
+    public synchronized List<ResultFrame> getFrameList(Node node) {
+        LOGGER.entering(CLASS_NAME, "getFrame");
+        
+        LinkedList<ResultFrame> resultFrameList = new LinkedList<ResultFrame>();
+        
+        for (ResultFrame resultFrame : frameList) {
+            if (resultFrame.frame.getSender().equals(node)) {
+                resultFrameList.add(resultFrame);
+            }
+        }
+        
+        LOGGER.exiting(CLASS_NAME, "getFrame", resultFrameList);
+        return resultFrameList;
+    }
+    
+    public synchronized List<ResultFrame> getFrameList() {
+        LOGGER.entering(CLASS_NAME, "getFrameList");
+        
+        LinkedList<ResultFrame> resultFrameList = new LinkedList<ResultFrame>(frameList);
+        
+        LOGGER.exiting(CLASS_NAME, "getFrameList", resultFrameList);
+        return resultFrameList;
+    }
+
+    public synchronized int countNodes() {
+        LOGGER.entering(CLASS_NAME, "countFrames");
+
+        int count = nodeList.size();
+        LOGGER.exiting(CLASS_NAME, "countFrames", count);
+        return count;
+    }
+
+    public synchronized Node getNode(int index) {
+        LOGGER.entering(CLASS_NAME, "getNode", index);
+
+        Node node = nodeList.get(index);
+        LOGGER.exiting(CLASS_NAME, "getNode", node);
+        return node;
+    }
+    
+    public synchronized List<Node> getNodeList() {
+        LOGGER.entering(CLASS_NAME, "getNodeList");
+        
+        LinkedList<Node> resultNodeList = new LinkedList<Node>(nodeList);
+        
+        LOGGER.exiting(CLASS_NAME, "getNodeList", resultNodeList);
+        return resultNodeList;
+    }
+    
+    public synchronized List<Node> getNodeList(EOJ eoj) {
+        LOGGER.entering(CLASS_NAME, "getNodeList", eoj);
+        
+        LinkedList<Node> resultNodeList = new LinkedList<Node>();
+        
+        for (Node node : nodeList) {
+            if (nodeEOJMap.get(node).contains(eoj)) {
+                resultNodeList.push(node);
+            }
+        }
+        
+        LOGGER.exiting(CLASS_NAME, "getNodeList", resultNodeList);
+        return resultNodeList;
+    }
+
+    public synchronized int countEOJs(Node node) {
+        LOGGER.entering(CLASS_NAME, "countEOJs", node);
+
+        LinkedList<EOJ> eojList = nodeEOJMap.get(node);
+        
+        if (eojList == null) {
+            LOGGER.exiting(CLASS_NAME, "countEOJs", 0);
+            return 0;
+        }
+        
+        int count = eojList.size();
+        LOGGER.exiting(CLASS_NAME, "countEOJs", count);
+        return count;
+    }
+
+    public synchronized EOJ getEOJ(Node node, int index) {
+        LOGGER.entering(CLASS_NAME, "getEOJ", new Object[]{node, index});
+
         LinkedList<EOJ> eojs = nodeEOJMap.get(node);
         
         if (eojs == null) {
             eojs = new LinkedList<EOJ>();
         }
         
-        if (!eojs.contains(eoj)) {
-            eojs.add(eoj);
-        }
-        
-        nodeEOJMap.put(node, eojs);
-    }
-    
-    public synchronized boolean addFrame(Frame frame) {
-        
-        if (!frame.getCommonFrame().isStandardPayload()) {
-            invalidFrames.add(frame);
-            return false;
-        }
-        
-        if (!(frame.getCommonFrame().getEDATA() instanceof StandardPayload)) {
-            invalidFrames.add(frame);
-            return false;
-        }
-        
-        Node node = frame.getSender();
-        StandardPayload payload = (StandardPayload)frame.getCommonFrame().getEDATA();
-        
-        if (payload.getESV() != ESV.Get_Res && payload.getESV() != ESV.Get_SNA) {
-            invalidFrames.add(frame);
-        }
-        
-        addEOJs(node, payload);
-        
-        if (payload.getESV() == ESV.Get_SNA) {
-            errorFrames.add(frame);
-        }
-        
-        return frames.add(frame);
-    }
-
-    public synchronized int countFrames() {
-        LOGGER.entering(CLASS_NAME, "countFrames");
-        
-        int count = frames.size();
-        LOGGER.exiting(CLASS_NAME, "countFrames", count);
-        return count;
-    }
-
-    public synchronized Frame getFrame(int index) {
-        LOGGER.entering(CLASS_NAME, "getFrame", index);
-        
-        Frame frame = frames.get(index);
-        LOGGER.exiting(CLASS_NAME, "getFrame", frame);
-        return frame;
-    }
-    
-    public synchronized int countNodes() {
-        LOGGER.entering(CLASS_NAME, "countFrames");
-        
-        int count = nodes.size();
-        LOGGER.exiting(CLASS_NAME, "countFrames", count);
-        return count;
-    }
-    
-    public synchronized Node getNode(int index) {
-        LOGGER.entering(CLASS_NAME, "getNode", index);
-        
-        Node node = nodes.get(index);
-        LOGGER.exiting(CLASS_NAME, "getNode", node);
-        return node;
-    }
-    
-    public synchronized int countEOJs(Node node) {
-        LOGGER.entering(CLASS_NAME, "countEOJs");
-        
-        LinkedList<EOJ> eojs = nodeEOJMap.get(node);
-        int count = eojs.size();
-        LOGGER.exiting(CLASS_NAME, "countEOJs", count);
-        return count;
-    }
-    
-    public synchronized EOJ getEOJ(Node node, int index) {
-        LOGGER.entering(CLASS_NAME, "getEOJ", index);
-        
-        LinkedList<EOJ> eojs = nodeEOJMap.get(node);
         EOJ eoj = eojs.get(index);
         LOGGER.exiting(CLASS_NAME, "getEOJ", eoj);
         return eoj;
+    }
+    
+    public synchronized List<EOJ> getEOJList(Node node) {
+        LOGGER.entering(CLASS_NAME, "getEOJList", node);
+        
+        LinkedList<EOJ> eojList = nodeEOJMap.get(node);
+        
+        if (eojList == null) {
+            eojList = new LinkedList<EOJ>();
+        }
+        
+        LinkedList<EOJ> resultList = new LinkedList<EOJ>(eojList);
+        LOGGER.exiting(CLASS_NAME, "getEOJList", resultList);
+        return resultList;
     }
 }
